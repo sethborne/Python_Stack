7b72c2be575407c3d6879a7dfd5b5fc76a739e2d
Seth Borne <sethborne@gmail.com>
2017-09-16 12:37:48 -0700

    Python - Fundamentals - Lesson 06 - Data Types - First Commit

---
+++ 01_PYTHON/B_PYTHON_FUNDAMENTALS/06_DATA_TYPES/06_DATA_TYPES.py
@@ -0,0 +1,14 @@
+# define a function that says hello to the name provided
+# this starts a new block
+
+def sayHello(name):
+    # these lines are indented therefore part of the function
+    if name:
+        print 'Hello, ' + name + ' from inside the function'
+    else:
+        print 'No Name'
+# now we're unidentified and have ended the previous block
+print 'Outside of the function'
+
+sayHello("Seth")
+
