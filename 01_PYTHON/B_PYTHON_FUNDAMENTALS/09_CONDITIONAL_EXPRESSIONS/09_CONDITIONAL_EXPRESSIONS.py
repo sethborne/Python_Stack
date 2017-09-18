fdbd628ff62b1e5aaf11d1e20b160c645ea1fb9e
Seth Borne <sethborne@gmail.com>
2017-09-17 17:09:10 -0700

    Python - Fundamentals - Lesson 09 - Conditional Expressions - First Commit

---
+++ 01_PYTHON/B_PYTHON_FUNDAMENTALS/09_CONDITIONAL_EXPRESSIONS/09_CONDITIONAL_EXPRESSIONS.py
@@ -0,0 +1,13 @@
+age = 20
+if age >= 18:
+    print "Legal Age"
+else:
+    print "You Are So Young!"
+    
+    
+if age >= 18:
+    print "Legal Age"
+elif age == 17:
+    print "You Are Seventeen"
+else:
+    print "You Are So Young!"
